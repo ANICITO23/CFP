f829b25f9b5601ff12b18f0c1df332fc8a7c748b
ANICITO23 <anicito812@gmail.com>
2023-09-18 00:38:44 -0500

    metodo get y post , creacion de tamplates asignacion de rutas

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+#sirve para crear un formulario
+from django.contrib.auth.forms import UserCreationForm
+#sirve para almacenar datos
+from django.contrib.auth.models import User
+#enviar un texto
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request,'signup.html',{
+        'form': UserCreationForm
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #registrar usuario
+            try:
+                user=User.objects.create_user(username=request.POST['username'],
+                                          password=request.POST['password1'])
+                user.save()
+                return HttpResponse('user creted successfuly')
+            except:
+                return HttpResponse('el usuario ya existe')
+        return HttpResponse('password do not much')
+
